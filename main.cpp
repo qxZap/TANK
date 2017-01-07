339c635a1cb1f2d9baa7cd03acb30862114ff9fe
qxZap <MileaMihai@outlook.it>
2017-01-07 16:52:24 +0200

    Extension till difficulty menu

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <windows.h>
 bool game=true;
 int menu;
 char Map[10][21][30];
 char Menu[2][21][30];
-
+int difficulty;
 using namespace std;
 void LoadResurces();
 void LoadMenu();
 void NewGame();
 void PrintMenu(int NumberMenu);
+void SelectTank();
 
 
 void LoadResurces()
@@ -34,6 +36,7 @@ void LoadResurces()
 
 void PrintMenu(int NumberMenu)
 {
+        system("CLS");
         for(int i=0;i<21;i++)
         {
             for(j=0;j<21;j++)
@@ -43,10 +46,62 @@ void PrintMenu(int NumberMenu)
 
 }
 
+void SelectTank()
+{
+    char input;
+    int position=0;
+    PrintMenu(position+6);
+    while(true)
+    {
+        PrintMenu(position+6);
+        cin>>input;
+        if(input)
+    }
+}
+
 void NewGame()
 {
     char input;
     int position=0;
+    PrintMenu(position+2);
+    while(true)
+    {
+
+        PrintMenu(position+2);
+        cin>>input;
+        if(input==NULL)
+        {
+            if(position==0)
+            {
+                difficulty=1;break;SelectTank();
+            }
+            else if(position==1)
+            {
+                difficulty=2;break;SelectTank();
+            }
+            else if(position==2)
+            {
+                difficulty=3;break;SelectTank();
+            }
+            else if(position==3)
+            {
+                break;LoadMenu();
+            }
+        }
+        else if(input=='s')
+        {
+            if(position==3)
+                position=0;
+            else position++;
+        }
+        else if(input=='w')
+        {
+            if(position==0)
+                position=3;
+            else position--;
+        }
+
+    }
 
 }
 
@@ -62,7 +117,7 @@ void LoadMenu()
             cin>>input;
             if(input==NULL)
             {
-                if(position==1)
+                if(position==0)
                     NewGame();
                 else
                     {break;game=false;}
@@ -81,6 +136,7 @@ void LoadMenu()
                 else
                     position--;
             }
+            PrintMenu(position);
         }
 }
 
