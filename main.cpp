843342259059da36124b5b68b8e5a7d087f2bab1
Milea Mihai-Cristian <MileaMihai@outlook.it>
2017-01-10 00:52:29 +0200

    Toate Fisierele+ Update Inainte de Movement
    csf

--- main.cpp
+++ main.cpp
@@ -2,35 +2,69 @@
 #include <fstream>
 #include <math.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <time.h>
 bool game=true;
 int menu;
-char Map[10][21][30];
-char Menu[2][21][30];
+char Map[12][21][30];
+char Menu[3][21][30];
 int difficulty;
+int tank;
+int play_map;
 using namespace std;
 void LoadResurces();
 void LoadMenu();
 void NewGame();
 void PrintMenu(int NumberMenu);
 void SelectTank();
-
+void SelectMap();
 
 void LoadResurces()
 {
     ifstream MenuGetNull("Menu0.txt");
-    for(int i=0;i<2;i++)
-        for(int j=0;j<30;j++)
-            MenuGetNull>>Menu[0][i][j];
-
     ifstream MenuGetOne("Menu1.txt");
+    ifstream MenuGetTwo("Menu2.txt");
+    ifstream MenuGetThree("Menu3.txt");
+    ifstream MenuGetFour("Menu4.txt");
+    ifstream MenuGetFive("Menu5.txt");
+    ifstream MenuGetSix("Menu6.txt");
+    ifstream MenuGetSeven("Menu7.txt");
+    ifstream MenuGetEight("Menu8.txt");
+    ifstream MenuGetNine("Menu9.txt");
+    ifstream MenuGetTen("Menu10.txt");
+    ifstream MenuGetOneOne("Menu11.txt");
+    ifstream MapGetNull("Map0.txt");
+    ifstream MapGetOne("Map1.txt");
+    ifstream MapGetTwo("Map2.txt");
+
     for(int i=0;i<21;i++)
         for(int j=0;j<30;j++)
-            MenuGetOne>>Menu[1][i][j];
+            {
+                //std::noskipws>>
+                MenuGetNull>>Menu[0][i][j];
+                MenuGetOne>>Menu[1][i][j];
+                MenuGetTwo>>Menu[2][i][j];
+                MenuGetThree>>Menu[3][i][j];
+                MenuGetFour>>Menu[4][i][j];
+                MenuGetFive>>Menu[5][i][j];
+                MenuGetSix>>Menu[6][i][j];
+                MenuGetSeven>>Menu[7][i][j];
+                MenuGetEight>>Menu[8][i][j];
+                MenuGetNine>>Menu[9][i][j];
+                MenuGetTen>>Menu[10][i][j];
+                MenuGetOneOne>>Menu[11][i][j];
+                MapGetNull>>Map[0][i][j];
+                MapGetOne>>Map[1][i][j];
+                MapGetTwo>>Map[2][i][j];
+            }
+
+}
 
 
-
-
-
+void SelectMap()
+{
+    srand (time(NULL));
+    play_map=rand() % 5 + 1;
 
 }
 
@@ -39,7 +73,7 @@ void PrintMenu(int NumberMenu)
         system("CLS");
         for(int i=0;i<21;i++)
         {
-            for(j=0;j<21;j++)
+            for(int j=0;j<30;j++)
                 cout<<Menu[NumberMenu][i][j];
             cout<<"\n";
         }
@@ -51,11 +85,49 @@ void SelectTank()
     char input;
     int position=0;
     PrintMenu(position+6);
-    while(true)
+    while(game)
     {
         PrintMenu(position+6);
         cin>>input;
-        if(input)
+        if(input=='q')
+        {
+            if(position==5)
+            {
+                break;
+                NewGame();
+            }
+            else
+            {
+                break;
+                tank=position;
+                SelectMap();
+            }
+        }
+        else if(input=='a')
+        {
+            if(position==0)
+            {
+                position=5;
+            }
+            else
+            {
+                position--;
+            }
+        }
+        else if(input=='d')
+        {
+            if(position==5)
+            {
+                position=0;
+            }
+            else
+            {
+                position++;
+            }
+        }
+
+
+
     }
 }
 
@@ -64,29 +136,25 @@ void NewGame()
     char input;
     int position=0;
     PrintMenu(position+2);
-    while(true)
+    while(game)
     {
 
         PrintMenu(position+2);
         cin>>input;
-        if(input==NULL)
+        if(input=='q')
         {
-            if(position==0)
+
+            if(position==3)
             {
-                difficulty=1;break;SelectTank();
+                LoadMenu();
             }
-            else if(position==1)
+            else
             {
-                difficulty=2;break;SelectTank();
+
+                difficulty=position;
+                SelectTank();
             }
-            else if(position==2)
-            {
-                difficulty=3;break;SelectTank();
-            }
-            else if(position==3)
-            {
-                break;LoadMenu();
-            }
+
         }
         else if(input=='s')
         {
@@ -111,20 +179,20 @@ void LoadMenu()
 
         char input;
         int position=0;
-        Print_Menu(0);
-        while(true)
+        PrintMenu(0);
+        while(game)
         {
             cin>>input;
-            if(input==NULL)
+            if(input=='q')
             {
                 if(position==0)
                     NewGame();
                 else
-                    {break;game=false;}
+                {game=false;exit(0);}
             }
             else if(input=='s')
             {
-                if(pisition==1)
+                if(position==1)
                     position--;
                 else
                     position++;
@@ -142,21 +210,11 @@ void LoadMenu()
 
 int main()
 {
-    menu=1;
-    LoadSources();
+    LoadResurces();
     while(game)
-    {
-        if(menu==1)
-        {
-            LoadMenu();
-        }
-        else
-        {
-
-        }
-
-    }
+        LoadMenu();
+    return 0;
 
 
-    return ;
+
 }
